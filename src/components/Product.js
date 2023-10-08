5b245fbd5ef4c2371e6634e65ddecf1c8999853c
saberali1789 <78509035+saberali1789@users.noreply.github.com>
2023-10-08 22:31:53 +0300

    some styles

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,11 +5,11 @@ const Product = (props) => {
   console.log(props);
   const product = props.product
   return (
-    <div className="card" >
+    <div className="card p-1 mb-5" >
     <img src={product.image} className="card-img-top img-card" alt='00' />
     <div className="card-body">
-      <h5 className="card-title">{product.title}</h5>
-      <p className="card-text">{product.description}</p>
+      <h5 className="card-title">{product.title.slice(0, 20)}</h5>
+      <p className="card-text">{product.description.slice(0, 70)}...</p>
       <a href="/#" className="btn btn-primary">Details</a>
     </div>
   </div>
